d959019ff31fbd6603231536c71bb07eaf29c431
sujal gawas <sujalgawas18@gmail.com>
2025-08-30 11:22:13 +0530

    browserRouter changed to HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.jsx
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';                              // ← NEW
 
 // Components
